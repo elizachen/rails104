4cbc09d37bafac953b2d80d3dac4b0ea658cd668
elizachen <elizachen34@gmail.com>
2016-12-18 14:02:39 +0800

    Add edit & destory function for account_posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @Post = Post.find(params[:id])
+    @Post.destroy
+    redirect_to account_posts_path, alert: "One post deleted"
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update the post successfully"
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
